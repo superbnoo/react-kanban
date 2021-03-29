d035acb85ae9205150f6900945896b3883a7140f
An Tran <tranvuan82@gmail.com>
2021-03-29 23:30:45 +0700

    implement draggable, droppable

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
-import dummyData from './dummy-data';
-import Column from './column';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
+import dummyData from "./dummy-data";
+import Column from "./column";
 
-// const App = () => 'hello world';
 
 class App extends React.Component {
   state = dummyData;
 
-  render() {
-    return this.state.columnOrder.map((columnId) => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+  onDragEnd = (result) => {
+    // todo
+  }
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    });
+  render() {
+    return (
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+      >
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
